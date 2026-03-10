688a22fac786948b0a6df601640118fd289bec5e
Goggy <bigggrinz@gmail.com>
2026-03-10 13:10:05 +0500

    первый коммит

--- src/main/ru/practicum/moviehub/model/Movie.java
+++ src/main/ru/practicum/moviehub/model/Movie.java
@@ -16,14 +16,26 @@ public class Movie {
         this.year = year;
     }
 
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    public Integer getYear() { return year; }
-    public void setYear(Integer year) { this.year = year; }
+    public Integer getYear() {
+        return year;
+    }
+    public void setYear(Integer year) {
+        this.year = year;
+    }
 
     @Override
     public boolean equals(Object o) {
